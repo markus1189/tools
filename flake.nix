3dd5600742c903511879ad627fc358b331b03aae
Markus Hauck <markus1189@gmail.com>
2026-01-30 15:31:49 +0100

    Add minimal flake.nix with taskwarrior

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "Development shell with taskwarrior";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in {
+      devShells = forAllSystems (system:
+        let pkgs = nixpkgs.legacyPackages.${system};
+        in {
+          default = pkgs.mkShell {
+            packages = [ pkgs.taskwarrior3 ];
+          };
+        });
+    };
+}
